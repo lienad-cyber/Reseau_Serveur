450d8907902ab53ae10737e4909659b3b7ea46b0
lienad-cyber <stevens.rakotomalala@esti.mg>
2024-11-30 10:17:53 +0300

    some improvements post

--- HTTP_SERVEUR/redoit/DifferentHttpError.java
+++ HTTP_SERVEUR/redoit/DifferentHttpError.java
@@ -1,4 +1,5 @@
 import com.sun.net.httpserver.HttpExchange;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
@@ -22,19 +23,10 @@ public class DifferentHttpError {
         }
     }
 
-    public void Error404(HttpExchange exchange, String message) throws IOException {
-        String FilePathPageNotFound = "../../htdocs/Error/Error404.html";
-
-        Path pathToFile = Paths.get(FilePathPageNotFound);
-        byte[] fileContentBytes = Files.readAllBytes(pathToFile);
-
-        exchange.getResponseHeaders().set("Content-Type", "text/html; charset=UTF-8");
-        exchange.sendResponseHeaders(404, fileContentBytes.length);
-
-        // La reponse du servera
-        try (
-                OutputStream os = exchange.getResponseBody()) {
-            os.write(fileContentBytes);
-        }
+    public static  void Error404(DataOutputStream writer) throws IOException {
+        writer.writeBytes("HTTP/1.1 404 Not Found\r\n");
+        writer.writeBytes("Content-Type: text/html\r\n");
+        writer.writeBytes("\r\n");
+        writer.writeBytes("<html><body><h1>Erreur 404 - Fichier non trouvé</h1></body></html>");
     }
 }
--- HTTP_SERVEUR/redoit/SocketServer.java
+++ HTTP_SERVEUR/redoit/SocketServer.java
@@ -1,9 +1,6 @@
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.*;
+import java.nio.file.Files;
 
 public class SocketServer {
     public void MyFileHandler(Socket ClientSocket) throws IOException {
@@ -33,6 +30,12 @@ public class SocketServer {
 
             if (requestedFile.endsWith("/")) {
                 displayListFile(requestedFile, writer);
+                // } else if (requestedFile.endsWith(".php")) {
+                // // Raha php
+                // handlePHPFile(requestedFile, writer);
+            } else {
+                // html, css, ...
+                handleStaticFile(requestedFile, writer);
             }
 
         } catch (Exception e) {
@@ -43,10 +46,30 @@ public class SocketServer {
         }
     }
 
+    public void handleStaticFile(String currrentPath, DataOutputStream write) throws IOException {
+        File file = new File("../../htdocs/" + currrentPath);
+
+        if (file.exists()) {
+            byte [] fileByte = Files.readAllBytes(file.toPath());
+            String contentType = getContentType(currrentPath);
+
+            write.writeBytes("HTTP/1.1 200 OK \r\n");
+            write.writeBytes("Content-Type : "+ contentType + "\r\n");
+            write.writeBytes("Content-Length : "+ fileByte.length + "\r\n");
+            write.writeBytes("\r\n");
+
+            write.write(fileByte);
+        } else {
+            // Error 404
+            DifferentHttpError.Error404(write);
+        }
+
+    }
+
     private void displayListFile(String currentPath, DataOutputStream writer) throws IOException {
         System.out.println("");
 
-        File currentFile = new File("../../htdocs/ " + currentPath);
+        File currentFile = new File("../../htdocs/" + currentPath);
 
         if (currentFile.exists()) {
             if (currentFile.isDirectory()) {
@@ -57,17 +80,19 @@ public class SocketServer {
                 htmlResponse.append("<ul>");
 
                 // Le fileParent
+                
+
 
                 if (listFiles != null) {
                     for (File file : listFiles) {
                         String filename = file.getName();
 
                         if (file.isDirectory()) {
-                            htmlResponse.append("<li><a href='").append(currentPath).append("/").append(filename)
-                                    .append("/").append("''>").append(filename).append("'</a></li>'");
+                            htmlResponse.append("<li><a href='").append(currentPath).append(filename)
+                                    .append("/").append("''>").append(filename).append("</a></li>");
                         } else {
-                            htmlResponse.append("<li><a href='").append(currentPath).append("/").append(filename)
-                                    .append("/").append("''>").append(filename).append("'</a></li>'");
+                            htmlResponse.append("<li><a href='").append(currentPath).append(filename)
+                                    .append("''>").append(filename).append("</a></li>");
                         }
                     }
                 }
@@ -86,4 +111,18 @@ public class SocketServer {
             // DifferentHttpError.Error404(HttpExchange exchange, String message);
         }
     }
+
+    private String getContentType(String fileName) {
+        String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+
+        return switch (extension) {
+            case "html" -> "text/html";
+            case "css" -> "text/css";
+            case "js" -> "application/javascript";
+            case "png" -> "image/png";
+            case "jpg", "jpeg" -> "image/jpeg";
+            default -> "application/octet-stream";
+        };
+    }
+
 }
